269abcacd07e71f61d8c78f6b5d921869289517a
Rajat Mehra <mehrarajat60@gmail.com>
2019-02-25 00:30:55 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -4,30 +4,35 @@ import { sampleData } from './demo.js'
 
 class App extends Component {
   render() {
-    const { audit = [] }= sampleData
+    const { audit = [] } = sampleData
     console.log(sampleData)
     return (
-      
-        <table border = "10">
-        <tr>
-            <th>Id</th>
-            <th>created at</th>
-            <th>event</th>
-            <th>email</th>
-        </tr>
-        {audit.map((item)=>{
-          return(
-            <tr>
-          <td>{}</td>
-          <td>{}</td>
-          <td></td>
-          <td></td>
-        </tr>
-          )
-        })}
-        
-        
-        </table>
+      <div className="table-audit">
+        <div>
+          <h1> Audit table of Company Employees</h1>
+          <table border="10">
+            <tbody>
+              <tr>
+                <th>Id</th>
+                <th>created at</th>
+                <th>event</th>
+                <th>email</th>
+              </tr>
+              {audit.map((item) => {
+                return (
+                  <tr>
+                    <td>{item.id}</td>
+                    <td>{item.created_at}</td>
+                    <td>{item.event}</td>
+                    <td>{item.user_email}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
     );
   }
 }
